13da9ce1e684780ea13f86dca5834f4439f3da73
richardilinkoln-coder <richardilinkoln@gmail.com>
2026-05-27 22:28:03 +0300

    Create admin.py

---
+++ bot/filters/admin.py
@@ -0,0 +1,11 @@
+# bot/filters/admin.py
+import os
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+
+class IsAdmin(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        # Просто впиши сюда ID админов напрямую вместо чтения из .env
+        admins = [123456789, 987654321]  # <-- Замени эти цифры на реальные TG ID
+        
+        return message.from_user.id in admins
