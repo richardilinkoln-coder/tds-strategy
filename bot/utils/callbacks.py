6d9b3219aab1544619811ce15f0e2435014a5a0e
richardilinkoln-coder <richardilinkoln@gmail.com>
2026-05-27 22:02:30 +0300

    Add HelperCB and ReviewCB classes for callbacks

--- bot/utils/callbacks.py
+++ bot/utils/callbacks.py
@@ -13,3 +13,17 @@ class PartyCB(CallbackData, prefix="party"):
 class NavCB(CallbackData, prefix="nav"):
     action: str
     strategy_id: str = "_"
+
+
+from aiogram.filters.callback_data import CallbackData
+
+# ... твои текущие классы ...
+
+class HelperCB(CallbackData, prefix="hlp"):
+    action: str  # 'list', 'card'
+    helper_id: int = 0
+
+class ReviewCB(CallbackData, prefix="rev"):
+    action: str  # 'start', 'save', 'all'
+    helper_id: int
+    stars: int = 0
